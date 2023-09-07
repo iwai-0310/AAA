e302dbf4ae71c5dc475ab0515ab7be40b2e24906
iwai-0310 <chazesingh@gmail.com>
2023-09-07 11:57:00 +0530

    added the leaflet api for the map feature

--- src/app/component/userdetail/userdetail.component.ts
+++ src/app/component/userdetail/userdetail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { UserService } from 'src/app/service/user.service';
 import {User} from '../../inteface/user.interface'
+import { Coordinate } from 'src/app/inteface/coordinate.interface';
+import * as Leaflet from 'leaflet'
 
 @Component({
   selector: 'app-userdetail',
@@ -13,12 +15,21 @@ export class UserdetailComponent implements OnInit {
   user:User
   mode:'edit' | 'locked'='locked'
   buttonText:'Save'|'Edit'='Edit'
+  marker=new Leaflet.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
+    iconSize: [32, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    shadowSize: [41, 41]
+  })
 
  constructor(private activatedRoute:ActivatedRoute,private userService:UserService){}
   
  ngOnInit(): void {
     this.user=(<User>(this.activatedRoute.snapshot.data['resolvedResponse'].results[0]))
     console.log(this.user)
+    this.loadMap(this.user.coordinate)
     // this.activatedRoute.paramMap.subscribe((params:ParamMap)=>{
     //   console.log('User ID:',params.get('uuid')!)
     //   this.userService.getUser(params.get('uuid')!).subscribe(
@@ -39,7 +50,23 @@ changeMode(mode?: 'edit' | 'locked'):void{
     console.log('Updating user on the backend')
   }
 }
-  
+  private loadMap(coordinate:Coordinate):void{
+    const map=Leaflet.map('map',{
+      center:[coordinate.latitude,coordinate.logitude],
+      zoom:5
+    })
+    const mainLayer=Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
+      tileSize:512,
+      zoomOffset:-1,
+      minZoom:1,
+      maxZoom:30,
+      crossOrigin:true,
+      attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+    mainLayer.addTo(map)
+    const marker=Leaflet.marker([coordinate.latitude,coordinate.logitude],{icon:this.marker})
+    marker.addTo(map).bindPopup(`${this.user.firstName}'s Location`).openPopup();
+  }
 
  
 }
