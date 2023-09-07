db804e93c4f555b284ac002967e7bb034627f1f2
iwai-0310 <chazesingh@gmail.com>
2023-09-07 10:48:32 +0530

    Created  a resolver to fix the error when the detail page loads for an empty value when the value os not loaded

--- src/app/component/userdetail/userdetail.component.ts
+++ src/app/component/userdetail/userdetail.component.ts
@@ -2,31 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { UserService } from 'src/app/service/user.service';
 import {User} from '../../inteface/user.interface'
-import * as Leaflet from 'leaflet'
-import { Coordinate } from 'src/app/inteface/coordinate.interface';
-import {Response} from '../../inteface/response.interface'
+
 @Component({
   selector: 'app-userdetail',
   templateUrl: './userdetail.component.html',
   styleUrls: ['./userdetail.component.css']
 })
 export class UserdetailComponent implements OnInit {
-  response:Response;
+  
+  user:User
   mode:'edit' | 'locked'='locked'
   buttonText:'Save'|'Edit'='Edit'
 
  constructor(private activatedRoute:ActivatedRoute,private userService:UserService){}
   
  ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe((params:ParamMap)=>{
-      console.log('User ID:',params.get('uuid')!)
-      this.userService.getUser(params.get('uuid')!).subscribe(
-        (response:any)=>{
-          console.log(response)
-          this.response=response
-        }
-      )
-    })
+    this.user=(<User>(this.activatedRoute.snapshot.data['resolvedResponse'].results[0]))
+    console.log(this.user)
+    // this.activatedRoute.paramMap.subscribe((params:ParamMap)=>{
+    //   console.log('User ID:',params.get('uuid')!)
+    //   this.userService.getUser(params.get('uuid')!).subscribe(
+    //     (response:any)=>{
+    //       console.log(response)
+    //       this.response=response
+    //     }
+    //   )
+    // })
   }
  
 changeMode(mode?: 'edit' | 'locked'):void{
--- src/app/service/user.resolver.ts
+++ src/app/service/user.resolver.ts
@@ -1,5 +1,21 @@
-import { ResolveFn } from '@angular/router';
+// import { ResolveFn } from '@angular/router';
+// import { Response } from '../inteface/response.interface';
 
-export const userResolver: ResolveFn<boolean> = (route, state) => {
-  return true;
-};
+import { Injectable } from "@angular/core";
+import { Response } from "../inteface/response.interface";
+import { ActivatedRoute, ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
+import { UserService } from "./user.service";
+import { Observable } from "rxjs/internal/Observable";
+
+// constructor
+// export const userResolver: ResolveFn<Response> = (route, state) => {
+  
+// };
+@Injectable({providedIn:'root'})
+export class userResolver implements Resolve<Response>{
+  constructor(private userService:UserService){}
+
+  resolve(route:ActivatedRouteSnapshot,_:RouterStateSnapshot):Observable<Response>{
+    return this.userService.getUser(route.paramMap.get('uuid')!)
+  }
+}
