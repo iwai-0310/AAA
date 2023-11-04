edb70141c5f1751efc28460237d8d770f94d7328
iwai-0310 <chazesingh@gmail.com>
2023-11-04 21:52:56 +0530

    creats the html div for our new map api

--- src/app/component/userdetail/userdetail.component.ts
+++ src/app/component/userdetail/userdetail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { UserService } from 'src/app/service/user.service';
-import {User} from '../../inteface/user.interface'
+import { User } from '../../inteface/user.interface'
 import { Coordinate } from 'src/app/inteface/coordinate.interface';
 import * as Leaflet from 'leaflet'
 
@@ -11,11 +11,11 @@ import * as Leaflet from 'leaflet'
   styleUrls: ['./userdetail.component.css']
 })
 export class UserdetailComponent implements OnInit {
-  
-  user:User
-  mode:'edit' | 'locked'='locked'
-  buttonText:'Save'|'Edit'='Edit'
-  marker=new Leaflet.Icon({
+
+  user: User
+  mode: 'edit' | 'locked' = 'locked'
+  buttonText: 'Save' | 'Edit' = 'Edit'
+  marker = new Leaflet.Icon({
     iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
     iconSize: [32, 41],
     iconAnchor: [12, 41],
@@ -24,10 +24,10 @@ export class UserdetailComponent implements OnInit {
     shadowSize: [41, 41]
   })
 
- constructor(private activatedRoute:ActivatedRoute,private userService:UserService){}
-  
- ngOnInit(): void {
-    this.user=(<User>(this.activatedRoute.snapshot.data['resolvedResponse'].results[0]))
+  constructor(private activatedRoute: ActivatedRoute, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.user = (<User>(this.activatedRoute.snapshot.data['resolvedResponse'].results[0]))
     console.log(this.user)
     this.loadMap(this.user.coordinate)
     // this.activatedRoute.paramMap.subscribe((params:ParamMap)=>{
@@ -40,33 +40,33 @@ export class UserdetailComponent implements OnInit {
     //   )
     // })
   }
- 
-changeMode(mode?: 'edit' | 'locked'):void{
-  console.log(mode)
-  this.mode=this.mode==='locked' ? 'edit':'locked'
-  this.buttonText=this.buttonText==='Edit' ? 'Save':'Edit'
-  if(mode==='edit'){
-    //Logic to update user on the backend
-    console.log('Updating user on the backend')
+
+  changeMode(mode?: 'edit' | 'locked'): void {
+    console.log(mode)
+    this.mode = this.mode === 'locked' ? 'edit' : 'locked'
+    this.buttonText = this.buttonText === 'Edit' ? 'Save' : 'Edit'
+    if (mode === 'edit') {
+      //Logic to update user on the backend
+      console.log('Updating user on the backend')
+    }
   }
-}
-  private loadMap(coordinate:Coordinate):void{
-    const map=Leaflet.map('map',{
-      center:[coordinate.latitude,coordinate.logitude],
-      zoom:5
+  private loadMap(coordinate: Coordinate): void {
+    const map = Leaflet.map('map', {
+      center: [coordinate.latitude, coordinate.logitude],
+      zoom: 5
     })
-    const mainLayer=Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
-      tileSize:512,
-      zoomOffset:-1,
-      minZoom:1,
-      maxZoom:30,
-      crossOrigin:true,
-      attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    const mainLayer = Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      tileSize: 512,
+      zoomOffset: -1,
+      minZoom: 1,
+      maxZoom: 30,
+      crossOrigin: true,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     })
     mainLayer.addTo(map)
-    const marker=Leaflet.marker([coordinate.latitude,coordinate.logitude],{icon:this.marker})
+    const marker = Leaflet.marker([coordinate.latitude, coordinate.logitude], { icon: this.marker })
     marker.addTo(map).bindPopup(`${this.user.firstName}'s Location`).openPopup();
   }
 
- 
+
 }
